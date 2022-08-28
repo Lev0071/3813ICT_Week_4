07a9da705817dfe396e5ff5157a6c7d6b2163ef7
Lev0071 <74816473+Lev0071@users.noreply.github.com>
2022-08-28 15:16:44 +1000

    Navigating Programmatically

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,25 @@
-import { Component, EventEmitter, Output } from "@angular/core";
+import { Component, EventEmitter, Output,OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
 
-export class HeaderComponent{
+export class HeaderComponent implements OnInit{
     @Output() sectionSelected = new EventEmitter<string>()
+
+    constructor(private router:Router){}
+
+    ngOnInit(): void {
+        
+    }
 
     onSelect(section:string){
         this.sectionSelected.emit(section);
+    }
+
+    onNavigate(route:string){
+        this.router.navigate(['/'+ route]);
     }
 }
